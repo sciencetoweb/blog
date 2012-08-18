8c65f33e1ed275575193b54bec2597f6bd7af5cf
Julien Maupetit <julien@maupetit.net>
2012-08-18 17:12:30 +0200

    First commit

---
+++ pelicanconf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Julien Maupetit"
+SITENAME = u"Science to Web"
+SITEURL = 'http://www.sciencetoweb.com'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+THEME = "notmyidea"
+
+# Blogroll
+LINKS =  (('Julien Maupetit', 'http://www.maupetit.net'),
+          ('Author 2', 'http://misc.org'),)
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/sciencetoweb'),)
+
+DEFAULT_PAGINATION = False
