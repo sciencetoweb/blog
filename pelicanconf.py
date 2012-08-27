e60c911c7c239c68f6eae6243b056a2590c6a20a
Julien Maupetit <julien@maupetit.net>
2012-08-28 01:36:34 +0200

    Adapted blog settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@
 AUTHOR = u"Julien Maupetit"
 SITENAME = u"Science to Web"
 SITEURL = 'http://www.sciencetoweb.com'
+SITESUBTITLE = u"Science and web technologies united"
 
 TIMEZONE = 'Europe/Paris'
 
@@ -11,10 +12,11 @@ DEFAULT_LANG = 'en'
 THEME = "newspaper"
 
 # Blogroll
-LINKS =  (('Julien Maupetit', 'http://www.maupetit.net'),
-          ('Author 2', 'http://misc.org'),)
+LINKS =  (('Julien Maupetit', 'http://www.maupetit.net'),)
 
 # Social widget
-SOCIAL = (('twitter', 'http://twitter.com/sciencetoweb'),)
+SOCIAL = (('Twitter', 'http://twitter.com/sciencetoweb'),)
 
+# Settings
+DISPLAY_PAGES_ON_MENU = False
 DEFAULT_PAGINATION = False
